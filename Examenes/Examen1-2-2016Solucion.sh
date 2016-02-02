34c89c93bb989818528db2c5186affd427dcae92
jesusninoc <jesusmnc@hotmail.es>
2016-02-03 00:10:45 +0100

    Update Examen1-2-2016Solucion.sh

--- Examenes/Examen1-2-2016Solucion.sh
+++ Examenes/Examen1-2-2016Solucion.sh
@@ -4,5 +4,3 @@
 #Crear un script en el que pida un nombre al usuario y si el nombre es 'pepito' que almacene dicho nombre en un fichero.
 
 #Recorrer un fichero que tiene nombres de personas y si la persona que está dentro del fichero es 'pepito' almacenar dicho nombre en un fichero.
-Status API Training Shop Blog About Pricing
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
