9b3674c8864b397264bb884374781bf776d9493f
jesusninoc <jesusmnc@hotmail.es>
2016-02-05 10:29:27 +0100

    Update Examen01-02-2016Solucion.sh

--- Examenes/Examen01-02-2016Solucion.sh
+++ Examenes/Examen01-02-2016Solucion.sh
@@ -1,3 +1,17 @@
 #Crear un script en el que pida un nombre al usuario y si el nombre es 'pepito' que almacene dicho nombre en un fichero.
 
+read nombre
+if [ $nombre == 'pepito' ]
+then
+echo "Nombre: "$nombre > $nombre.txt
+fi
+
 #Recorrer un fichero que tiene nombres de personas y si la persona que está dentro del fichero es 'pepito' almacenar dicho nombre en un fichero.
+
+while read nombre
+do
+if [ $nombre == 'pepito' ]
+then
+echo "Nombre: "$nombre > $nombre.txt
+fi
+done < ficheronombres.txt
