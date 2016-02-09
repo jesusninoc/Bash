c7c3cf46482a161a1c52a68ea5a526da769d51ee
jesusninoc <jesusmnc@hotmail.es>
2016-02-09 22:35:56 +0100

    Update Examen04-02-2016Enunciado.sh

--- Examenes/Examen04-02-2016Enunciado.sh
+++ Examenes/Examen04-02-2016Enunciado.sh
@@ -1,1 +1,3 @@
 #Realizar un programa que permita pedir al usuario un PID de proceso y si es mayor de 5 que se pare el proceso.
+
+#Realizar un programa que permita pedir al usuario un nombre de proceso y si el PID del proceso es mayor de 5 que se pare.
