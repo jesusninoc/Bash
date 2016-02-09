e701c4c10f541ebfc5c4f8cf0ae7c1421389ea36
jesusninoc <jesusmnc@hotmail.es>
2016-02-09 22:35:37 +0100

    Update Examen04-02-2016Solucion.sh

--- Examenes/Examen04-02-2016Solucion.sh
+++ Examenes/Examen04-02-2016Solucion.sh
@@ -1,5 +1,4 @@
 #Realizar un programa que permita pedir al usuario un PID de proceso y si es mayor de 5 que se pare el proceso.
-
 echo 'Introduzca PID de proceso que quiere parar: '
 read npid
 if [ $npid -ge 5 ]
@@ -9,7 +8,6 @@ kill -9 $npid
 fi
 
 #Realizar un programa que permita pedir al usuario un nombre de proceso y si el PID del proceso es mayor de 5 que se pare.
-
 echo 'Introduzca nombre de proceso que quiere parar: '
 read npro
 npids=`ps -ef | grep $npro | awk '{print $2}'`
