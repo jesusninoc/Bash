f02f71526dfb0fe4f68e41348a0f8d37b71ce30d
jesusninoc <jesusninoc@users.noreply.github.com>
2016-04-29 21:25:57 +0200

    Create Examen26-04-2016Enunciado.sh
    Simulacro examen liberatorio Linux

---
+++ Examenes/Examen26-04-2016Enunciado.sh
@@ -0,0 +1,30 @@
+#Explica cada apartado
+
+#Apartado 1
+
+adb shell ip link
+adb shell ip address
+adb shell ip addrlabel
+adb shell ip neighbour
+adb shell ip route
+adb shell ip rule
+adb shell ip maddress
+adb shell ip mroute
+adb shell ip tunnel
+adb shell ip xfrm state
+ 
+adb shell ifconfig
+ 
+adb shell netstat
+adb shell netstat -n
+adb shell netstat -anp
+adb shell netstat -p
+adb shell netstat -a
+
+#Apartado 2
+
+sudo fdisk -l
+df -h
+sudo blkid
+sudo mkdir mount_name
+sudo mount -t auto -v /dev/sdb1 /mnt/mount_name
