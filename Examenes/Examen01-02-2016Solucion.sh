75b5e2649eaa3df4cf3a960e74b752ece5635c6e
jesusninoc <jesusninoc@users.noreply.github.com>
2017-01-17 23:53:29 +0100

    Update Examen01-02-2016Solucion.sh

--- Examenes/Examen01-02-2016Solucion.sh
+++ Examenes/Examen01-02-2016Solucion.sh
@@ -1,6 +1,6 @@
 #Crear un script en el que pida un nombre al usuario y si el nombre es 'pepito' que almacene dicho nombre en un fichero.
 read nombre
-if [ $nombre == 'pepito' ]
+if [ $nombre = 'pepito' ]
 then
 echo "Nombre: "$nombre > $nombre.txt
 fi
@@ -8,7 +8,7 @@ fi
 #Recorrer un fichero que tiene nombres de personas y si la persona que está dentro del fichero es 'pepito' almacenar dicho nombre en un fichero.
 while read nombre
 do
-if [ $nombre == 'pepito' ]
+if [ $nombre = 'pepito' ]
 then
 echo "Nombre: "$nombre > $nombre.txt
 fi
