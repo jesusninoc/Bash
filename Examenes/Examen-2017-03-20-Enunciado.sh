d35b199d281009fffc63b9359492302ed22e3798
jesusninoc <jesusninoc@users.noreply.github.com>
2017-04-12 09:05:24 +0200

    Create Examen-2017-03-20-Enunciado.sh

---
+++ Examenes/Examen-2017-03-20-Enunciado.sh
@@ -0,0 +1,53 @@
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#HAZ CAPTURAS DE TODO LO QUE HAGAS
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#Pedir un proceso en Linux: Comprobar la ruta y el hash (hay un listado de hash para los programas que hay en el sistema). Comprobar si hay algún drivers cambiado (hay un listado de hash para los drivers).
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#Instalar programas en Debian (Ubuntu) leyendo de un fichero los nombres de los programas.
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#Crear usuarios si no existen, la contraseñas y el nombre se leen de un fichero, si tienes tiempo puedes meter la creación de grupos y añadir usuarios a grupos.
+
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#TAREAS
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#¿Se te ocurre un sistema para comprobar que se ha ejecutado un programa?
+#Pasos para realizar el examen:
+#Planifica la solución, paso a paso, por ejemplo en la tarea:
+#	1. Crea una confirmación simple * * * * * echo "hola" >> fichero
+#	2. Comprueba que funciona
+#	3. Crea un script sencillo un echo
+#	4. Ejecuta correctamente (sh script.sh o chmod u+x script.sh)
+#	5. Mete el script en el cron
+#	6. Comprueba que funciona el script en el cron
+#	6. Complica el script y resuelve la situación que te pido (1, 2, 3, 4 o 5)
+#Cuando hagas un ejercicio me lo puedes enseñar
+#---------------------------------------------------------------------------------------------------
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
+#1. Crear un fichero con el contenido de todos los procesos (toda la información)
+#2. Crear un fichero con el contenido de todos los PID de los procesos
+#3. Crear un fichero con el contenido del PID del proceso nano (abrir nano)
+#4. Comprobar si el hash de un fichero coincide con el que tiene el proceso (no se ha modificado)
+
+#5. Leer de un fichero al que se le van incorporando líneas y extraer de cada línea el sueldo.
+#Ejemplo de fichero:
+#1 Juan Director 1000
+#2 Pepe Sistemas 4000
+#3 Marta Contabilidad 2000
+
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+TEN EN CUENTA:
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+#Seguridad en contraseñas
+#Crear usuarios
+#Permisos a la hora de ejecutar los scripts
