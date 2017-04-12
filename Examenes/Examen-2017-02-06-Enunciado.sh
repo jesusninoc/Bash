537e7547f30bc81eb1bb3dd3202968406f9c8d76
jesusninoc <jesusninoc@users.noreply.github.com>
2017-04-12 11:58:29 +0200

    Create Examen-2017-02-06-Enunciado.sh

---
+++ Examenes/Examen-2017-02-06-Enunciado.sh
@@ -0,0 +1,5 @@
+#Como usuario pedro crea un fichero con nombre topsecret.txt al que únicamente él tenga acceso, tanto de lectura como de escritura.
+#Crea otro fichero, también como usuario pedro, con nombre ventas_trimestre.txt al que tengan acceso, tanto para leer como para escribir todos los usuarios que pertenezcan al mismo grupo. Comprueba como usuario pablo que puedes modificar el fichero.
+#Como usuario alba, crea un fichero con nombre empleados.txt al que pueda acceder cualquier usuario para leer su contenido, y cualquier usuario del mismo grupo para leer o escribir.
+#Como usuario pablo, copia un programa del directorio /usr/bin al directorio de trabajo con un nombre diferente. Por ejemplo kalarm se puede copiar como alarma. Mira los permisos de este programa. Comprueba que se puede ejecutar.
+#Cambia los permisos de alarma de tal forma que sólo lo pueda ejecutar el propietario del archivo.
