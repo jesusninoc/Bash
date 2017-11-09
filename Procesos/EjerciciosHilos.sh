e72a2348230cefc5bf1f8681d0eaf11497990092
Jesús <jesnino@users.noreply.github.com>
2017-11-09 09:19:23 +0100

    Update EjerciciosHilos.sh

--- Procesos/EjerciciosHilos.sh
+++ Procesos/EjerciciosHilos.sh
@@ -1,7 +1,13 @@
 #Conocer el PID de un proceso
 ps -ef | grep nano
 
-#Averiguar el número de hilos usados por ese proceso.
+#Ver todos los hilos de los procesos
+ps -efL
+
+#Ver información sobre el proceso
+cat /proc/(PID)/status
+
+#Seleccionar la información sobre el proceso relacionada con Hilos
 cat /proc/(PID)/status | grep Threads
 
 #TODO JUNTO
