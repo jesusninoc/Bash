83e9189c8bbcdf005518da047793d468b38a8cc5
Jesús <jesnino@users.noreply.github.com>
2017-11-26 16:39:06 +0100

    Create EjemplosDescomprimir.sh

---
+++ Ficheros/EjemplosDescomprimir.sh
@@ -0,0 +1,23 @@
+
+#- Descomprimir .tgz
+$ tar -xvzf archivo.tgz
+#- Descomprimir .tar
+$ tar -xvf archivo.tar
+#- Descomprimir .bz2
+$ bzip2 -d fichero.bz2
+#- Descomprimir .tar.bz2
+$ tar jxvf archivo.tar.bz2
+#- Descomprimir .gz
+$ gzip -d fichero.gz
+#- Descomprimir .tar.gz
+$ tar zxf archivo.tar.gz
+#- Descomprimir .rar
+$ rar x archivo.rar
+#- Descomprimir .lha
+$ lha x archivo.lha
+#- Descomprimir .zip
+$ unzip archivo.zip
+#- Descomprimir .arj
+$ unarj archivo.arj o $ arj x archivo.arj
+#- Descomprimir .zoo
+$ zoo x archivo.zoo
