fbaacbec5e716e0f29f2458faff47cf9f9ed77bf
Jesús <16318863+jesusninoc@users.noreply.github.com>
2018-01-20 10:17:42 +0100

    Create UtilizarCadenas.sh

---
+++ Repaso/UtilizarCadenas.sh
@@ -0,0 +1,26 @@
+#Contar letras que hay en una palabra
+echo $(echo ..... | wc -c)
+
+#Contar palabras
+echo $(echo ..... | wc -w)
+
+#Numerar líneas de un fichero
+nl /etc/passwd
+
+#Campo 1 de un fichero
+cut -f1 -d:
+
+#Mostrar los usuarios seguidos
+cut -f1 -d: /etc/passwd | xargs
+
+#Ordenar por un campo
+sort -k -t: /etc/passwd
+
+#Buscar un usuario concreto
+cat /etc/passwd | grep "usuario"
+
+#Reemplazar el caracter h por el caracter r
+echo "hola" | tr 'h' 'r'
+
+#Eliminar un caracter
+echo "hola" | tr -d 'a'
