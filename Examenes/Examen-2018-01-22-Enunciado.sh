7443eb547283827447e08f7c3b3bd8918554058b
Jesús <16318863+jesusninoc@users.noreply.github.com>
2018-01-22 23:29:44 +0100

    Update Examen-2018-01-22-Enunciado.sh

--- Examenes/Examen-2018-01-22-Enunciado.sh
+++ Examenes/Examen-2018-01-22-Enunciado.sh
@@ -1,13 +1,16 @@
-#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
-
 #1. Agregar a un fichero información sobre software que está instalado en el sistema, actualizaciones y procesos que se están ejecutando.
 
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
 #2. Crear un fichero con el contenido de todos los procesos (toda la información).
 
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
 #3. Crear un fichero con el contenido de todos los PID de los procesos.
 
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
 #4. Crear un fichero con el contenido del PID del proceso nano (abrir nano).
 
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es (crear un log que añada el contenido):
 #5. Comprobar si el hash de un fichero coincide con el que tiene el proceso (no se ha modificado).
 
+#Programar una tarea cada cinco minutos, la tarea que hay que programas es:
 #6. Resolver mediante una tarea en el cron: sumar el valor de un fichero cada minuto.
